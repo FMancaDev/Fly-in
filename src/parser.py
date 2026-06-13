767f2cab08d748578340e879ab1000d731673df8
Fode Manca <fomanca@c1r15s1.42porto.com>
2026-06-13 22:09:36 +0100

    feat: initial of parsing

---
+++ src/parser.py
@@ -0,0 +1,11 @@
+class ParserError(Exception):
+    def __init__(self, message: str, line_number: int):
+        super().__init__(f"Line {line_number}: {message}")
+
+
+class Parser:
+    def parse(self, filepath: str) -> Graph:
+        pass
+
+    def _parse_metadata(self, raw: str) -> dict[str, str]:
+        pass
