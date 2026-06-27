bed54044e95ac575b251b9da6993bc52b228406f
Fode Manca <fomanca@c2r2s2.42porto.com>
2026-06-27 21:04:41 +0100

    refactor: testing algorithm

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.parser import Parser
+from src.pathfinding import Pathfinder
 
 
 def main() -> None:
     parser = Parser()
 
-    graph, nb_drones = parser.parse("maps/test.map")
+    graph, nb_drones = parser.parse(
+        "maps/medium/02_circular_loop.txt")
 
     print("=== GRAPH ===")
     print(f"Drones: {nb_drones}")
@@ -22,6 +24,20 @@ def main() -> None:
     print("\nConnections:")
     for connection in graph.connections:
         print(connection)
+
+    Path_finder = Pathfinder()
+
+    path = Path_finder.find_path(
+        graph, graph.start_hub, graph.end_hub
+    )
+
+    print("\nShortest path:")
+
+    if not path:
+        print("No path found")
+    else:
+        for hub in path:
+            print(hub.name)
 
 
 if __name__ == "__main__":
